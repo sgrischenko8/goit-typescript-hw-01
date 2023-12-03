d9aeba50c56e0897e177bf26191317f48890f8dd
sgrischenko8 <120989472+sgrischenko8@users.noreply.github.com>
2023-12-03 09:00:04 +0000

    Deploying to gh-pages from @ sgrischenko8/goit-typescript-hw-01@ec42edf3c0e961af01fa96f16fa85e0ab2f79502 🚀

---
+++ index.js
@@ -0,0 +1,11 @@
+import { concatenation } from './concatenation';
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+if (button && input) {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        concatenation(input.value, 'hello!');
+        input.value = "";
+    });
+}
+//# sourceMappingURL=index.js.map
